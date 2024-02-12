30ba63b0254428b749c360619d5100d7e8cf8e00
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2024-02-12 06:31:27 -0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Vijaivir Dhaliwal on 2024-02-12.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
