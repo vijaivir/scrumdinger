9b160d1872386e9fdbcc3b009e0931fc079ff2f5
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2024-02-14 09:20:34 -0800

    init scrumdinger

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Vijaivir Dhaliwal on 2024-02-12.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 10, total:15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+
+                }
+            }
+            .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            
+            Circle().strokeBorder(lineWidth: 20)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}){
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
