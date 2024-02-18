478593d9891fdbdf624a8ab280b3bf0f3439c9d0
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2024-02-17 21:13:17 -0800

    state and binding

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,53 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Vijaivir Dhaliwal on 2024-02-17.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
